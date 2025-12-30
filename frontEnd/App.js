218668d571bf4d21b0977111aba7bfbb60a0863e
sivasanjaycse <sivasanjaidisco@gmail.com>
2025-12-30 17:55:27 +0530

    Initial Commit

---
+++ frontEnd/App.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Siva Sanjay's new App</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
